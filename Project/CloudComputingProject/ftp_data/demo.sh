50215ed575262fbedee987be977939465b2ca97c
Madheena30 <n.munavvarah@gmail.com>
2024-12-17 12:45:47 +0530

    Add files via upload

---
+++ Project/CloudComputingProject/ftp_data/demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Enter your name :"
+read name
+echo "Enter your age :"
+read age
+echo "Enter your number :"
+read number
+echo "Enter your department :"
+read department
+echo "enter your filename :"
+read file
+echo "your name is $name"
+echo "your age is $age"
+echo "your number is $number"
+echo "your are from $department department"
+echo "your file name is $file"
+
+
+
+
+
